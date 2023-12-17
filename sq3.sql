8c0d4b8a09c4274411fb2117c3d62b1a0822bee3
Fabbye3737 <fabian.besca01@e-uvt.ro>
2023-12-17 09:16:14 +0200

    Ex3

---
+++ sq3.sql
@@ -0,0 +1,46 @@
+CREATE TABLE IF NOT EXISTS tara(
+    id serial not null,
+    name varchar not null,
+    primary key(id)
+);
+
+CREATE TABLE IF NOT EXISTS structura(
+    id serial not null,
+    tip varchar not null UNIQUE,
+    primary key(id)
+);
+
+CREATE TABLE IF NOT EXISTS marime(
+    id serial not null,
+    marime varchar not null UNIQUE,
+    primary key(id)
+);
+
+CREATE TABLE IF NOT EXISTS vulnerabilitate(
+    id serial not null,
+    vulnerabilitate varchar not null UNIQUE,
+    primary key(id)
+);
+
+-- Add the tara_id column to the tara table
+ALTER TABLE tara ADD COLUMN tara_id integer;
+
+CREATE TABLE IF NOT EXISTS regiune_geografica(
+    structura_id integer not null,
+    marime_id integer not null,
+    vulnerabilitate_id integer not null,
+    nume varchar not null,
+    primary key(structura_id, marime_id, vulnerabilitate_id),
+    foreign key(structura_id) references structura(id),
+    foreign key(marime_id) references marime(id),
+    foreign key(vulnerabilitate_id) references vulnerabilitate(id),
+    foreign key(tara_id) references tara(id)
+);
+
+CREATE TABLE IF NOT EXISTS uat(
+    id serial not null,
+    name varchar not null,
+    tara_id integer not null,
+    primary key(id),
+    foreign key(tara_id) references tara(id)
+);
